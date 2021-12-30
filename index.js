2a0eb9b638dd82a945fd26c236727598c3f11990
Mayank2@14 <55187056+mayank2021@users.noreply.github.com>
2021-12-30 16:26:02 +0530

    add files

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import _ from 'lodash';
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+
+
+mongoose.connect("mongodb+srv://heymayank12:heymayank12@cluster0.1meyy.mongodb.net/blogDB", {useNewUrlParser:true});
+
+const blogSchema = {
+    postTitle:String,
+    postBody:String
+};
+
+const Blog = mongoose.model("Blog",blogSchema);
+
+app.get("/api",(req,res) => {
+    Blog.find()
+    .then(blogs => res.json(blogs))
+    .catch(err => res.status(400).json(err));
+});
+
+app.post("/api/compose", (req, res) => {
+ const userPost = new Blog ({
+     postTitle:req.body.title,
+     postBody:req.body.post
+ });
+
+userPost.save()
+.then(() => console.log("Added item successfully"))
+.catch((err) => console.log(err));
+});
+
+
+app.post("/api/delete",(req,res) => {
+    Blog.deleteOne({_id:req.body._id}, (err) => {
+        if(err) console.log(err)
+        else console.log("Delete Item successfully");
+    })
+});
+
+app.post("/api/update", (req,res) => {
+ console.log(req.body);
+ Blog.updateOne({_id:req.body._id},{postTitle:req.body.postTitle,postBody:req.body.postBody},(err) => {
+     if(err) console.log(err);
+     else console.log("Updated Successfully");
+ })
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+app.listen(3001 , () => {
+    console.log("server up and running at 3001");
+})
